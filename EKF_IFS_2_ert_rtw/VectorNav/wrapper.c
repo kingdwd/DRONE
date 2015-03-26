1be1075f19aaf95f3a5b8a18a9825460b6dab53b
tharunprasanth <vivekanandan@ku.edu>
2015-03-25 21:02:17 -0500

    change in VN baudrate

--- EKF_IFS_2_ert_rtw/VectorNav/wrapper.c
+++ EKF_IFS_2_ert_rtw/VectorNav/wrapper.c
@@ -27,10 +27,10 @@
  * Public Definitions
  **************************************************************************/
 const char* const COM_PORT = "//dev//ttyUSB0";
-const int VN_BAUDRATE = 9600;
+const int VN_BAUDRATE = 115200;
 
 
-#define ARDUINO_COM "/dev/ttyACM1"
+#define ARDUINO_COM "/dev/ttyACM0"
 #define AR_BAUDRATE B115200  /*rate for usb serial*/
 
 #define PDEBUG(lvl, fmt, ...)						\
