ba56c6587343eced0ad506f8ad14b7f7790277ee
tharunprasanth <vivekanandan@ku.edu>
2015-04-10 00:55:10 -0500

    small modificcation in pwm code

--- pwm_generator/pwm_generator.ino
+++ pwm_generator/pwm_generator.ino
@@ -2,6 +2,7 @@ int throttle = 11;
 int elevator= 10;
 int aileron = 06;
 int rudder = 05;
+int i;
 void setup()
 {
   pinMode(throttle,OUTPUT);
@@ -13,32 +14,39 @@ void setup()
 }
 void loop()
 
-{ 
+{   
+
   Serial.println("enter your choice");
   Serial.println("1- throttle : 2 -elevator  : 3 - rudder  :  4 - aileron");
   delay(200);
   while(Serial.available()==0){}
 
   int command =  Serial.parseInt();
+  Serial.flush();
   Serial.println("enter the value : 0-1024");
-  delay(200);
+  //delay(200);
   while(Serial.available()==0){}
   int val =  Serial.parseInt();
+  Serial.flush();
   val = map(val, 0, 1024, 0, 255);
   Serial.println(val);
   switch(command)
   {
-  case 1:
+  case 1:  while(Serial.available()==0)
            analogWrite(throttle,val);
+           i=Serial.parseInt();
            break;
-  case 2:
+  case 2:  while(Serial.available()==0)
            analogWrite(elevator,val);
+           i=Serial.parseInt();
            break;
-  case 3:
+  case 3:  while(Serial.available()==0)
            analogWrite(aileron,127);
+           i=Serial.parseInt();
            break;
-  case 4:
+  case 4:  while(Serial.available()==0)
            analogWrite(rudder,127);
+           i=Serial.parseInt();
            break;
   default:
            Serial.println("command mismatch");
